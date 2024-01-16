97c2f5f914b98966993781626e9687d2c31e4ef1
Oleksii Borovykov <borovikovvaleksey@gmail.com>
2024-01-16 20:22:37 +0200

    individual meal page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,5 +1,30 @@
-export default function MealsSlug ({ params }) {
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMealById } from '@/lib/meals';
+
+export default async function MealSlug ({ params }) {
+  const meal = await getMealById(params.mealSlug);
+  const instructions = meal.instructions.replaceAll('\n', '<br />')
+
   return (
-    <>Meals slug {params.slug}</>
-  )
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill alt={meal.slug} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+          __html: instructions
+        }} />
+      </main>
+    </>
+  );
 }
