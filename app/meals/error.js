ef4cd6f7991a8716e19fdce63d7bc1c44ff83e84
Oleksii Borovykov <borovikovvaleksey@gmail.com>
2024-01-16 12:39:06 +0200

    processed errors

---
+++ app/meals/error.js
@@ -0,0 +1,9 @@
+'use client'
+export default function MealsError() {
+  return (
+    <main className="error">
+      <h1>An error occured!</h1>
+      <p>Please, try again.</p>
+    </main>
+  )
+}
